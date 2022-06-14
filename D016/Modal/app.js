51aba67fd6a99a48aaa1133deb6936235d7de896
vinh.nguyenquoc <vinh.nguyenquoc3@ncc.asia>
2022-06-14 22:16:11 +0700

    pushcode

--- D016/Modal/app.js
+++ D016/Modal/app.js
@@ -3,10 +3,10 @@ var btns = document.querySelectorAll(".btn");
 function toggleAccordion() {
   btns.forEach((btn) => {
     btn.addEventListener("click", function () {
-
       const idElement = this.getAttribute("data-bs-target");
       const contentElement = document.querySelector(idElement);
       contentElement.classList.toggle("show");
+
     });
   });
 }
